700c27fec1811e0ab5337683e96e32d9234d0a4a
Estrella <2357417306@qq.com>
2023-01-11 16:57:43 +0800

    multi_thread mod

---
+++ src/client.h
@@ -0,0 +1,8 @@
+//
+// Created by Estrella on 2023/1/11.
+//
+
+#ifndef PROJECT_2_SRC_CLIENT_H_
+#define PROJECT_2_SRC_CLIENT_H_
+
+#endif //PROJECT_2_SRC_CLIENT_H_
---
+++ src/server.h
@@ -0,0 +1,8 @@
+//
+// Created by Estrella on 2023/1/11.
+//
+
+#ifndef PROJECT_2_SRC_SERVER_H_
+#define PROJECT_2_SRC_SERVER_H_
+
+#endif //PROJECT_2_SRC_SERVER_H_
